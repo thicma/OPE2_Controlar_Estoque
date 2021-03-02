383fd811aa0b346b2833e23cc597b53f93d00bab
thicma <48813355+thicma@users.noreply.github.com>
2021-03-01 21:40:54 -0300

    Primeiro Alteração

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+SECRET_KEY = 'alura'
+
+MYSQL_HOST = "127.0.0.1"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "admin"
+MYSQL_DB = "jogoteca"
+MYSQL_PORT = 3306
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
