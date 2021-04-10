8a1f96512ea3a789b7fd220467f33ca02142c12a
thicma <48813355+thicma@users.noreply.github.com>
2021-04-10 13:47:13 -0300

    alterações

--- FlaskMarket/market/forms.py
+++ FlaskMarket/market/forms.py
@@ -15,13 +15,14 @@ class RegisterForm(FlaskForm):
         if email_address:
             raise ValidationError('Email Address alredy exists! Please try a different email.')
 
-    username= StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
-    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    username= StringField(label='Usuário:', validators=[Length(min=2, max=30), DataRequired()])
+    name = StringField(label='Nome Completo', validators=[Length(min=2, max=30), DataRequired()])
+    charge = StringField(label='Cargo: ', validators=[Length(min=2, max=15), DataRequired()])
     password = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     password_confirm = PasswordField(label='Confirm Password:', validators=[EqualTo('password'), DataRequired()])
-    submit = SubmitField(label='Create Account')
+    submit = SubmitField(label='Criar Usuário')
 
 class LoginForm (FlaskForm):
-    username = StringField(label='User Name:', validators=[DataRequired()])
-    password = PasswordField(label='Password:', validators=[DataRequired()])
-    submit = SubmitField(label='Sign in:')
+    username = StringField(label='Usuário:', validators=[DataRequired()])
+    password = PasswordField(label='Senha:', validators=[DataRequired()])
+    submit = SubmitField(label='Entrar')
