8f44641a0c02f98355300b1f8959febbd353336f
thicma <48813355+thicma@users.noreply.github.com>
2021-04-10 12:23:34 -0300

    alteração de modelos

---
+++ FlaskMarket/market/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'd1e8ab4c46d1d1d40c592ef2'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+
+from market import routes
+
+
